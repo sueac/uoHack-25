d99fd23698642e2946326d09e6d95fd9cdbacead
Wwwang650 <wwwang650@gmail.com>
2026-01-17 10:26:16 -0500

    funcbetter

--- test.py
+++ test.py
@@ -3,16 +3,44 @@ def QuoteExtraction(Filename):
     with open(Filename,'r') as file:
         text=file.read()
     quotes=[]
-    temp=""
-    for char in text:
-        if char == '"':
-            if inside_quotes:
-                quotes.append(current)
-                current = ""
-                inside_quotes = False
-            else:
-                inside_quotes = True
-        elif inside_quotes:
-            current += char
+    nar=[]
+    i=0
+    n= len(text)
+    desc=["said","says","asked","added","remarked","replied","answered","whispered","mumbled","muttered","shouted","yelled","screamed"]
+    while i < n:
+        if text[i] == '"':
+            start = i + 1
+            end = start
+            while end < n and text[end] != '"':
+                end += 1
 
-    return quotes
+            quote = text[start:end]
+
+            before = text[max(0, i - 50):i]
+            after = text[end + 1:min(n, end + 50)]
+
+            speaker = None
+
+            before_words = before.split()
+            for j in range(len(before_words) - 1):
+                if before_words[j][0].isupper() and before_words[j + 1] in verbs:
+                    speaker = before_words[j]
+                    break
+
+            if speaker is None:
+                after_words = after.split()
+                for j in range(len(after_words) - 1):
+                    if after_words[j] in verbs and after_words[j + 1][0].isupper():
+                        speaker = after_words[j + 1]
+                        break
+
+            if speaker is not None:
+                dialogue.append((speaker, quote))
+            else:
+                unknown.append(quote)
+
+            i = end + 1
+        else:
+            i += 1
+
+    return dialogue, unknown
